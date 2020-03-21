50aa6de421eaa7042329f1b51a141003bc96d7d0
Alexandr629-prog <aaskaldovic@gmail.com>
2020-03-21 13:26:23 +0300

    start task

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 module.exports = function reverse (n) {
-    var temp=str.length;
-    var newstr="";
-    for(var i=temp-1; i>=0; i--)
-   { newstr+=str[i];}
-    return newstr;
+  return n.split("").reverse().join("");
+
   }
 
